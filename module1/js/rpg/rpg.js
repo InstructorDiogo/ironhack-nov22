45ba226b4071d6e8a0857638179e8206aafe3778
Instructor Diogo <instructordiogo@air-de-diogo-2.lan>
2022-12-15 20:39:00 +0000

    RPG piece of code

---
+++ module1/js/rpg/rpg.js
@@ -0,0 +1,61 @@
+class Player {
+
+  constructor(hp, attackValue, name) {    
+    this.hp = hp
+    this.attackValue = attackValue
+    this.name = name
+    console.log("The player was spawned.")
+  }
+
+  attack(target){
+    
+      target.hp -= this.attackValue
+      console.log(`${target.name} lost ${this.attackValue} hp and stil has ${target.hp} remaining.`)
+        
+  }
+}
+
+class Monster {
+
+  constructor(hp, attackValue, name)
+  {
+    this.hp = hp
+    this.attackValue = attackValue
+    this.name = name
+  }
+
+  attack(target){
+    
+      target.hp -= this.attackValue
+      console.log(`${target.name} lost ${this.attackValue} hp and stil has ${target.hp} remaining.`)
+  }
+}
+
+class Slime extends Monster {
+  constructor(hp, attackValue, name)
+  {
+    super(hp, attackValue, name)
+    this.hasExploded = false
+  }
+
+  explode(target){
+    if (!this.hasExploded)
+    {
+      target.hp -= 10
+      this.hp -= 10
+      console.log(`The slime exploded and ${target.name} lost 10hp.`)
+      this.hasExploded = true
+    }
+    else
+    {
+      console.log(`The slime already exploded and can't explode now.`)
+    }
+  }
+}
+
+let mainPlayer = new Player(20, 3, "Dani")
+let monster1 = new Slime(40, 3, 'Purple Slimerino')
+let monster2 = new Monster(30, 1, "Goblin")
+
+mainPlayer.attack(monster1)
+monster1.attack(mainPlayer)
